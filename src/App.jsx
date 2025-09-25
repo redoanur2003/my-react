9858f991768803d39bbdeab75750312f792d1672
Redoanur <redoanur03@gmail.com>
2025-09-25 17:39:52 +0600

    Creating and initializing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './countries/country/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>My react projects</h1>
+
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/countries/country/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>In the country</h3>
+        </div>
+    );
+};
+
+export default Countries;
